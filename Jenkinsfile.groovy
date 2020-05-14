48fe739cdf0dc71640c9982f7de4fed6b386a28b
SuzyKimTech <47144575+SuzyKimTech@users.noreply.github.com>
2020-05-14 14:19:21 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Git Process') {
             steps {
                 git credentialsId: 'd1818738-f9c0-499c-9136-f96c0df70a25',
-                        url: 'https://github.com/SuzyKimTech/SpringBoot_Jenkins.git'
+                url: 'https://github.com/SuzyKimTech/SpringBoot_Jenkins.git'
             }
         }
         stage('Build') {
